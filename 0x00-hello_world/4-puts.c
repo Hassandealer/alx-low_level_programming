ef22d42bd42f523eba39c36221846c0db25ac32a
Hassandealer <hsaumam@gmail.com>
2022-09-12 19:25:33 -0700

    task 4-6 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Printing
- *
- * return: always 0 (success)
- */
+* main - Printing
+*
+* Return: Always 0 (Successful)
+*/
 int main(void)
 {
-	puts("\ "Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
