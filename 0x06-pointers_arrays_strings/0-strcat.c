975e5ccf6d6a13243da829efb35bd164ab07f542
Hassandealer <hsaumam@gmail.com>
2022-09-22 07:59:11 -0700

    task 0 commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,32 @@
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
-* _strncat - Concatenates two strings using at most                                                                                 
-*            an inputted number of bytes from src.                                                                                  
-* @dest: The string to be appended upon.                                                                                            
-* @src: The string to be appended to dest.                                                                                          
-* @n: The number of bytes from src to be appended to dest.                                                                          
-*                                                                                                                                   
-* Return: A pointer to the resulting string dest.                                                                                   
-*/                                                                                                                                  
-char *_strncat(char *dest, char *src, int n)                                                                                         
-{                                                                                                                                    
-	int index = 0, dest_len = 0;                                                                                                 
+#include "main.h"
 
-	while (dest[index++])                                                                                                       
-		dest_len++;                                                                                                          
-			                                                        
-	for (index = 0; src[index] && index < n; index++)                       
-		dest[dest_len++] = src[index];                                                                                       
+/**
+**_strcat - function commute srtings
+* @dest: param pointer to a char
+* @src: param pointer to a char
+* Return: return value of dest
+*/
 
-	return (dest);                                                                                                               
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
 
